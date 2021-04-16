44d8ec00895a369ccf5c56b0a230b6617271dc47
259134lavanyabs <81295980+259134lavanyabs@users.noreply.github.com>
2021-04-16 06:19:22 +0530

    Update declfile.h

--- 3_Implementation/inc/declfile.h
+++ 3_Implementation/inc/declfile.h
@@ -1,1 +1,17 @@
+/**
+* @file declfile.h
+* @author your name
+* @brief
+* @version 0.1
+*
+* @copyright Copyright (c) 2021
+**/
+
+# ifndef __DECLFILE_H__
+# define __DECLFILE_H__
+/**
+* @brief function to add the student information
+*
+*/
+
 
